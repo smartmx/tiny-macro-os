b15aec77dfddb0d301cf9ab86de984e6d3218be7
CoderNotCute <30313912+smartmx@users.noreply.github.com>
2023-04-13 08:36:48 +0800

    add list and memb library.

---
+++ memb.c
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2004, Swedish Institute of Computer Science.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ *
+ * Author: Adam Dunkels <adam@sics.se>
+ *
+ */
+
+#include <string.h>
+#include "memb.h"
+
+/*---------------------------------------------------------------------------*/
+void
+memb_init(struct memb *m)
+{
+  memset(m->used, 0, m->num);
+  memset(m->mem, 0, m->size * m->num);
+}
+/*---------------------------------------------------------------------------*/
+void *
+memb_alloc(struct memb *m)
+{
+  int i;
+
+  for(i = 0; i < m->num; ++i) {
+    if(m->used[i] == false) {
+      /* If this block was unused, we set the used flag on
+     and return a pointer to the memory block. */
+      m->used[i] = true;
+      return (void *)((char *)m->mem + (i * m->size));
+    }
+  }
+
+  /* No free block was found, so we return NULL to indicate failure to
+     allocate block. */
+  return NULL;
+}
+/*---------------------------------------------------------------------------*/
+int
+memb_free(struct memb *m, void *ptr)
+{
+  int i;
+  char *ptr2;
+
+  /* Walk through the list of blocks and try to find the block to
+     which the pointer "ptr" points to. */
+  ptr2 = (char *)m->mem;
+  for(i = 0; i < m->num; ++i) {
+    if(ptr2 == (char *)ptr) {
+      /* We've found the block to which "ptr" points, so we check the allocation
+         status to detect the double-free error and free the block. */
+      if (m->used[i] == false)
+        return -1;
+      m->used[i] = false;
+      return 0;
+    }
+    ptr2 += m->size;
+  }
+  return -1;
+}
+/*---------------------------------------------------------------------------*/
+int
+memb_inmemb(struct memb *m, void *ptr)
+{
+  return (char *)ptr >= (char *)m->mem &&
+    (char *)ptr < (char *)m->mem + (m->num * m->size);
+}
+/*---------------------------------------------------------------------------*/
+int
+memb_numfree(struct memb *m)
+{
+  int i;
+  int num_free = 0;
+
+  for(i = 0; i < m->num; ++i) {
+    if(m->used[i] == false) {
+      ++num_free;
+    }
+  }
+
+  return num_free;
+}
+/** @} */
---
+++ memb.h
@@ -0,0 +1,165 @@
+/*
+ * Copyright (c) 2023, smartmx - smartmx@qq.com
+ * Copyright (c) 2004, Swedish Institute of Computer Science.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ *
+ * Author: Adam Dunkels <adam@sics.se>
+ *
+ */
+
+/**
+ * \addtogroup mem
+ * @{
+ */
+
+
+/**
+ * \defgroup memb memb: Memory block management functions
+ *
+ * The memory block allocation routines provide a simple yet powerful
+ * set of functions for managing a set of memory blocks of fixed
+ * size. A set of memory blocks is statically declared with the
+ * MEMB() macro. Memory blocks are allocated from the declared
+ * memory by the memb_alloc() function, and are deallocated with the
+ * memb_free() function.
+ *
+ * @{
+ */
+
+#ifndef _MEMB_H_
+#define _MEMB_H_
+
+#include <stdbool.h>
+
+
+#define MEMB_CONCAT2(s1, s2) s1##s2
+/**
+ * A C preprocessing macro for concatenating two preprocessor tokens.
+ *
+ * We need use two macros (MEMB_CONCAT and MEMB_CONCAT2) in order to allow
+ * concatenation of two \#defined macros.
+ */
+#define MEMB_CONCAT(s1, s2) MEMB_CONCAT2(s1, s2)
+#define MEMB_CONCAT_EXT_2(s1, s2) MEMB_CONCAT2(s1, s2)
+
+/**
+ * A C preprocessing macro for concatenating three preprocessor tokens.
+ */
+#define MEMB_CONCAT3(s1, s2, s3) s1##s2##s3
+#define MEMB_CONCAT_EXT_3(s1, s2, s3) MEMB_CONCAT3(s1, s2, s3)
+
+/**
+ * Declare a memory block.
+ *
+ * This macro is used to statically declare a block of memory that can
+ * be used by the block allocation functions. The macro statically
+ * declares a C array with a size that matches the specified number of
+ * blocks and their individual sizes.
+ *
+ * Example:
+ \code
+MEMB(connections, struct connection, 16);
+ \endcode
+ *
+ * \param name The name of the memory block (later used with
+ * memb_init(), memb_alloc() and memb_free()).
+ *
+ * \param structure The name of the struct that the memory block holds
+ *
+ * \param num The total number of memory chunks in the block.
+ *
+ */
+#define MEMB(name, structure, num) \
+        static unsigned char MEMB_CONCAT(name,_memb_used)[num]; \
+        static structure MEMB_CONCAT(name,_memb_mem)[num]; \
+        struct memb name = {sizeof(structure), num, \
+                                          MEMB_CONCAT(name,_memb_used), \
+                                          (void *)MEMB_CONCAT(name,_memb_mem)}
+
+struct memb {
+  unsigned short size;
+  unsigned short num;
+  unsigned char  *used;
+  void *mem;
+};
+
+#define MEMB_EXTERN(name)   extern struct memb name;
+
+/**
+ * Initialize a memory block that was declared with MEMB().
+ *
+ * \param m A set of memory blocks previously declared with MEMB().
+ */
+void  memb_init(struct memb *m);
+
+/**
+ * Allocate a memory block from a block of memory declared with MEMB().
+ *
+ * \param m A set of memory blocks previously declared with MEMB().
+ */
+void *memb_alloc(struct memb *m);
+
+/**
+ * Deallocate a memory block from a memory block previously declared
+ * with MEMB().
+ *
+ * \param m m A set of memory blocks previously declared with MEMB().
+ *
+ * \param ptr A pointer to the memory block that is to be deallocated.
+ *
+ * \return error code, should be 0 if successfully deallocated or -1 if the
+ * pointer "ptr" did not point to a legal memory block.
+ */
+int  memb_free(struct memb *m, void *ptr);
+
+/**
+ * Check if a given address is within a memory area previously
+ * declared with MEMB().
+ *
+ * \param m m A set of memory blocks previously declared with MEMB().
+ *
+ * \param ptr A pointer to the address to check
+ *
+ * \return 1 if the address is part of the memory block; otherwise 0
+ */
+int memb_inmemb(struct memb *m, void *ptr);
+
+/**
+ * Count free memory blocks
+ *
+ * \param m m A set of memory blocks previously declared with MEMB().
+ *
+ * \return the number of free (available) memory blocks
+ */
+int  memb_numfree(struct memb *m);
+
+/** @} */
+/** @} */
+
+#endif /* _MEMB_H_ */
